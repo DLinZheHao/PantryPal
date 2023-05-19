dac5258b69c970e2209d31a328fa5e9521129a66
DLinZheHao <BDLinOrsWJ0528@gmail.com>
2023-05-19 16:14:55 +0800

    first commit

---
+++ PantryPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PantryPal
+//
+//  Created by 林哲豪 on 2023/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PantryPal' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PantryPal
+  pod 'SwiftLint'
+  pod 'MJRefresh'
+end
