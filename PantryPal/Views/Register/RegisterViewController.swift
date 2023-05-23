a7bbfcd880b8ce332d40c02354e98b7146011876
DLinZheHao <BDLinOrsWJ0528@gmail.com>
2023-05-23 15:35:16 +0800

    Create homepage, registration page, login interface

---
+++ PantryPal/Views/Register/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  PantryPal
+//
+//  Created by 林哲豪 on 2023/5/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ PantryPal/Views/SignIn/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  PantryPal
+//
+//  Created by 林哲豪 on 2023/5/23.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
